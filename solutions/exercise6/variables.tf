8052cbc0ed6214e52eafd53efb620641988a8a14
Chris Matteson <chris.matteson@gmail.com>
2019-04-15 17:17:59 -0700

    now added outputs

--- solutions/exercise6/variables.tf
+++ solutions/exercise6/variables.tf
@@ -1,17 +1,11 @@
 # variables.tf – commonly configured parameters for our environment (i.e. projectName)
 
-#################################################
 # Azure Location
 variable "location" {
   default = "eastus"
 }
 
-#################################################
-# AWS Region
-variable "aws_region" {
-  default = "us-east-2"
-}
-
+# Vault Binary URL
 variable vault_url {
   description = "URL to download Vault Enterprise"
   default     = "https://releases.hashicorp.com/vault/1.0.0/vault_1.0.0_linux_amd64.zip"
