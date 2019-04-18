fb54b217f04d77a73cf1d9655a919101840cd104
Chris Matteson <chris.matteson@gmail.com>
2019-04-17 21:37:38 -0700

    added links for module docs

--- solutions/exercise5/main.tf
+++ solutions/exercise5/main.tf
@@ -75,24 +75,12 @@ data "template_file" "setup" {
   }
 }
 
-# AzureAD resources
-resource "azuread_application" "vaultapp" {
-  name = "${random_id.project_name.hex}-vaultapp"
-}
-
-resource "azuread_service_principal" "vaultapp" {
-  application_id = "${azuread_application.vaultapp.application_id}"
-}
-
-resource "azuread_service_principal_password" "vaultapp" {
-  service_principal_id = "${azuread_service_principal.vaultapp.id}"
-  value                = "${random_id.client_secret.id}"
-  end_date             = "2020-01-01T01:02:03Z"
-  depends_on           = ["azurerm_role_assignment.role_assignment"]
-}
-
-resource "azurerm_role_assignment" "role_assignment" {
-  scope              = "${data.azurerm_subscription.subscription.id}"
-  role_definition_id = "${data.azurerm_subscription.subscription.id}${data.azurerm_role_definition.role_definition.id}"
-  principal_id       = "${azuread_service_principal.vaultapp.id}"
+# Service Principal Module
+module "vaultapp" {
+  source             = "./modules/service_principal"
+  resource_group     = "${azurerm_resource_group.main.id}"
+  location           = "${azurerm_resource_group.main.location}"
+  project_name       = "${random_id.project_name.hex}"
+  subscription_id    = "${data.azurerm_client_config.current.subscription_id}"
+  role_definition_id = "${data.azurerm_role_definition.role_definition.id}"
 }
---
+++ solutions/exercise5/service_principal/variables.tf
@@ -0,0 +1,16 @@
+# variables.tf – commonly configured parameters for our environment (i.e. projectName)
+
+# Azure Resource Group
+variable "resource_group" {}
+
+# Azure Location
+variable "location" {}
+
+# Project Name
+variable "project_name" {}
+
+# Azure Subscription ID
+variable subscription_id {}
+
+# Role Definition ID
+variable role_definition_id {}
